86725181e6775d5f6782f48cb4b1c3a7f56bc76d
André Erdmann <dywi@mailerd.de>
2016-06-01 15:25:14 +0200

    abc.loggable: __doc__

--- kernelconfig/abc/loggable.py
+++ kernelconfig/abc/loggable.py
@@ -8,9 +8,33 @@ __all__ = ["AbstractLoggable"]
 
 
 class AbstractLoggable(object, metaclass=abc.ABCMeta):
+    """Base class for objects with a logger.
+
+    @cvar DEFAULT_LOGGER_NAME:  default logger name,
+                                the class name is used if empty/None
+    @type DEFAULT_LOGGER_NAME:  C{str} or C{None}
+
+    @ivar logger:  logger
+    @type logger:  L{logging.Logger}
+    """
+
     DEFAULT_LOGGER_NAME = None
 
     def __init__(self, *, logger=None, logger_name=None, parent_logger=None):
+        """Constructor.
+
+        @keyword logger:         logger, if this parameter is not None,
+                                 then logger_name and parent_logger will be
+                                 ignored. Defaults to None.
+        @type    logger:         C{logging.Logger} or C{None}
+        @keyword logger_name:    logger name, used when creating a new one
+        @type    logger_name:    C{str} or C{None}
+        @keyword parent_logger:  parent logger, which will be used to create
+                                 a child logger if logger is not set.
+                                 Defaults to None.
+        @type    parent_logger:  L{logging.Logger} or C{None}
+        """
+
         super(AbstractLoggable, self).__init__()
         self.logger = None
         self.set_logger(
