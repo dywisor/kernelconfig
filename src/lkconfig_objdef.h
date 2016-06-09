7839af6b8fe3fca2eaaabc66ffe27d2d0e4e5c44
André Erdmann <dywi@mailerd.de>
2016-06-09 18:46:34 +0200

    lkc bindings: add logging functions
    Provides (limited) access to logging.Logger objects
    in the lkc bindings.

--- src/lkconfig_objdef.h
+++ src/lkconfig_objdef.h
@@ -8,6 +8,17 @@
 
 #define LKCONFIG_PYMOD_NAME  "kernelconfig.kconfig.lkconfig"
 
+enum {
+    lkconfig_debug,
+    lkconfig_info,
+    lkconfig_warning,
+    lkconfig_error,
+    lkconfig_critical,
+
+    lkconfig__log_level_count
+};
+
+
 #define lkconfig_SymbolViewName  "SymbolView"
 typedef struct {
     PyObject_HEAD
@@ -33,5 +44,57 @@ static PyObject* lkconfig_ExprViewObject_new_from_struct (
     const struct expr* kconfig_expr
 );
 
+
+/**
+ * log function, va_list variant.
+ *
+ * If logger is NULL ("not set"), returns immediately w/ success status code.
+ *
+ * Otherwise, builds a Python string from format and vargs,
+ * and calls the logger's debug, info, warning, error or critical method,
+ * depending on the log_level parameter.
+ *
+ * @param logger       logger, may be NULL
+ * @param log_level    log level, must be one of lkconfig_{debug,...}
+ * @param format       log message format string
+ * @param vargs        log message varargs
+ *
+ * @return 0 => success, non-zero => failure
+ * */
+static int lkconfig_logv (
+    PyObject* const logger,
+    const int log_level,
+    const char* const format,
+    va_list vargs
+);
+
+/**
+ * log function, varargs variant.
+ *
+ * See lkconfig_logv().
+ * */
+__attribute__((format (printf, 3, 4)))
+static int lkconfig_log (
+    PyObject* const logger,
+    const int log_level,
+    const char* const format,
+    ...
+);
+
+
+/*
+ * PyUnicode_FromFormatV(fmt, vargs) does not need to be NULL-terminated
+
+#define lkconfig_log(logger, log_level, ...)  \
+    _lkconfig_log((logger), (log_level), __VA_ARGS__, NULL)
+*/
+
+/**
+ * log function, message string variant.
+ *
+ * See lkconfig_logv().
+ * */
+#define lkconfig_logs(logger, log_level, s)  \
+    lkconfig_log((logger), (log_level), "%s", (s))
 
 #endif  /* _KERNELCONFIG_LKCONFIG_OBJDEF_H_ */
--- src/lkconfig_utilfuncs.c
+++ src/lkconfig_utilfuncs.c
@@ -15,3 +15,56 @@ static int lkconfig_dict_add_str_x_int (
 
     return dictadd_ret;
 }
+
+
+static int lkconfig_logv (
+    PyObject* const logger,
+    const int log_level,
+    const char* const format,
+    va_list vargs
+) {
+    static const char* meth_names[lkconfig__log_level_count] = {
+        "debug", "info", "warning", "error", "critical"
+    };
+    PyObject* msg;
+    PyObject* logret;
+
+    if ( logger == NULL ) { return 0; }
+
+    if ( (log_level < 0) || (log_level >= lkconfig__log_level_count) ) {
+        PyErr_Format (
+            PyExc_ValueError, "unknown log level %d", log_level
+        );
+        return -1;
+    }
+
+    msg = PyUnicode_FromFormatV ( format, vargs );
+    if ( msg == NULL ) { return -1; }
+
+    logret = PyObject_CallMethod ( logger, meth_names[log_level], "O", msg );
+
+    Py_DECREF ( msg );
+
+    if ( logret == NULL ) {
+        return -1;
+    }
+
+    Py_DECREF ( logret );
+    return 0;
+}
+
+static int lkconfig_log (
+    PyObject* const logger,
+    const int log_level,
+    const char* const format,
+    ...
+) {
+    va_list vargs;
+    int ret;
+
+    va_start ( vargs, format );
+    ret = lkconfig_logv ( logger, log_level, format, vargs );
+    va_end ( vargs );
+
+    return ret;
+}
