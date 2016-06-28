76dca8ffffaf9785e318342889048789687c7f21
André Erdmann <dywi@mailerd.de>
2016-06-28 17:05:26 +0200

    config sources: adopt changes
    The (parsed) settings -> conf source object conversion
    is now done by the conf source class.

--- kernelconfig/sources/_sources.py
+++ kernelconfig/sources/_sources.py
@@ -3,7 +3,6 @@
 
 import abc
 import enum
-import functools
 import os.path
 import shlex
 
@@ -60,7 +59,10 @@ class ConfigurationSourcesBase(_sources_abc.AbstractConfigurationSources):
             source_info=source_info
         )
 
-    def get_configuration_basis_from_settings(self, settings):
+    def create_source_by_name(self, source_name):
+        raise NotImplementedError("curated sources")
+
+    def get_configuration_source_from_settings(self, settings):
         def join_subtype_and_args(subtype, args):
             return args if subtype is None else ([subtype] + args)
         # ---
@@ -91,105 +93,77 @@ class ConfigurationSourcesBase(_sources_abc.AbstractConfigurationSources):
 
         source_def = shlex.split(source_def_str)
 
+        source_cls = None
+        source_args = None
+        # source_data is already set
+        source_subtype = None
+
         # fixme: file uri
         if os.path.isabs(source_def[0]):
             # implicit file
-            return self.get_configuration_basis_from_file(
-                None, source_def, source_data
-            )
-        # --
-
-        source_type_name = source_def[0].lower()
-        source_type, source_subtype = (
-            self.SOURCE_TYPE_KEYWORDS.get(source_type_name)
-        )
-
-        if source_type is None:
-            assert source_subtype is None
-            # then the type is implicit, and we have to "guess":
-            #
-            # (a) is source_def[0] the name of a curated source?
-            #
-            # (b) is source_def[0] the name of a defconfig target?
-            #
-            # (c) file path of any type?
-            #
-            raise NotImplementedError("guess typeof", source_def)
-
-        elif source_type is ConfigurationSourceType.s_file:
-            return self.get_configuration_basis_from_file(
-                source_subtype, source_def[1:], source_data
-            )
-
-        elif source_type is ConfigurationSourceType.s_command:
-            return self.get_configuration_basis_from_command(
-                source_subtype, source_def[1:], source_data
-            )
-
-        elif source_type is ConfigurationSourceType.s_make:
-            # make command w/ target source_subtype
-            return self.get_configuration_basis_from_make(
-                source_subtype, source_def[1:], source_data
-            )
-
-        elif source_type is ConfigurationSourceType.s_source:
-            return self.get_configuration_basis_from_source(
-                source_subtype, source_def[1:], source_data
-            )
-
-        elif source_type is ConfigurationSourceType.s_script:
-            # embedded script w/ lang source_subtype
-            return self.get_configuration_basis_from_script(
-                source_subtype, source_def[1:], source_data
-            )
+            source_cls = _source.LocalFileConfigurationSource
+            source_args = source_def
 
         else:
+            source_args = source_def[1:]
+
+            source_type_name = source_def[0].lower()
+            source_type, source_subtype = (
+                self.SOURCE_TYPE_KEYWORDS.get(source_type_name)
+            )
+
+            if source_type is None:
+                assert source_subtype is None
+                # then the type is implicit, and we have to "guess":
+                #
+                # (a) is source_def[0] the name of a curated source?
+                #
+                # (b) is source_def[0] the name of a defconfig target?
+                #
+                # (c) file path of any type?
+                #
+                raise NotImplementedError("guess typeof", source_def)
+
+            elif source_type is ConfigurationSourceType.s_file:
+                source_cls = None
+
+            elif source_type is ConfigurationSourceType.s_command:
+                source_cls = None
+
+            elif source_type is ConfigurationSourceType.s_make:
+                # make command w/ target source_subtype
+                source_cls = _source.MakeConfigurationSource
+
+            elif source_type is ConfigurationSourceType.s_source:
+                source_cls = None
+
+            elif source_type is ConfigurationSourceType.s_script:
+                # embedded script w/ lang source_subtype
+                source_cls = None
+            # --
+        # --
+
+        if source_cls is None:
             raise NotImplementedError(
                 "unknown source type {}".format(source_type)
             )
+
+        return source_cls.new_from_settings(
+            conf_source_env=self.senv,
+            subtype=source_subtype, args=source_args, data=source_data,
+            parent_logger=self.logger
+        )
+    # --- end of get_configuration_source_from_settings (...) ---
+
+    def get_configuration_basis_from_settings(self, settings):
+        conf_source, conf_args = (
+            self.get_configuration_source_from_settings(settings)
+        )
+
+        return conf_source.get_configuration_basis(conf_args)
     # --- end of get_configuration_basis_from_settings (...) ---
 
-    @abc.abstractmethod
-    def get_configuration_basis_from_file(self, subtype, args, data):
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def get_configuration_basis_from_command(self, subtype, args, data):
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def get_configuration_basis_from_make(self, subtype, args, data):
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def get_configuration_basis_from_source(self, subtype, args, data):
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def get_configuration_basis_from_script(self, subtype, args, data):
-        raise NotImplementedError()
-
 # --- end of ConfigurationSourcesBase ---
-
-
-def _meth_no_subtype(unbound_method):
-    def wrapper(self, subtype, args, data):
-        if subtype is not None:
-            raise ValueError("methpd does not accept a subtype")
-        return unbound_method(self, subtype, args, data)
-
-    return functools.update_wrapper(wrapper, unbound_method)
-# ---
-
-
-def _meth_no_data(unbound_method):
-    def wrapper(self, subtype, args, data):
-        if data:
-            raise ValueError("methpd does not accept data")
-        return unbound_method(self, subtype, args, data)
-
-    return functools.update_wrapper(wrapper, unbound_method)
-# ---
 
 
 class ConfigurationSources(ConfigurationSourcesBase):
@@ -209,36 +183,3 @@ class ConfigurationSources(ConfigurationSourcesBase):
 
         "source":       (ConfigurationSourceType.s_source, None)
     }
-
-    def create_source_by_name(self, source_name):
-        raise NotImplementedError("curated sources")
-
-    @_meth_no_data
-    @_meth_no_subtype
-    def get_configuration_basis_from_file(self, subtype, args, data):
-        # FIXME:  file may be a relpath (-> installinfo//multdir)
-        #         or a remote path (-> urllib)
-        #         Use a FileConfigurationSource object
-        return args[0]
-
-    @_meth_no_data
-    @_meth_no_subtype
-    def get_configuration_basis_from_command(self, subtype, args, data):
-        raise NotImplementedError()
-
-    @_meth_no_data
-    def get_configuration_basis_from_make(self, subtype, args, data):
-        src = self.create_loggable(
-            _source.MakeConfigurationSource,
-            None
-        )
-        return src.get_configuration_basis(self.senv, subtype, args)
-    # ---
-
-    @_meth_no_data
-    @_meth_no_subtype
-    def get_configuration_basis_from_source(self, subtype, args, data):
-        raise NotImplementedError()
-
-    def get_configuration_basis_from_script(self, subtype, args, data):
-        raise NotImplementedError()
