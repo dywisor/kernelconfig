449401996d0b2448ed9873ba72128894a36325df
André Erdmann <dywi@mailerd.de>
2016-06-28 17:03:51 +0200

    "make" config source: use new base class

--- kernelconfig/sources/source/mk.py
+++ kernelconfig/sources/source/mk.py
@@ -3,19 +3,28 @@
 
 import os
 
-from ..abc import source as _source_abc
+from . import _base
 from ..abc import exc
-
-from ...util import subproc
-from ...util import fs
 
 
 __all__ = ["MakeConfigurationSource"]
 
 
-class MakeConfigurationSource(_source_abc.AbstractConfigurationSource):
+class MakeConfigurationSource(_base.CommandConfigurationSourceBase):
+    """
+    Configuration source that creates a .config file with "make <target>".
+    """
 
-    def get_configuration_basis(self, csenv, subtype, args):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.base_argv = []
+        self.make_target = None
+
+    def init_from_settings(self, subtype, args, data):
+        if data:
+            raise exc.ConfigurationSourceInvalidError("non-empty data")
+        # --
+
         # FIXME: for defconfig,
         #        this needs some investigation and minor changes
         #
@@ -29,46 +38,103 @@ class MakeConfigurationSource(_source_abc.AbstractConfigurationSource):
         #        it would good to choose the best-fitting one!
         #
 
-        cmdv = ["make", "-C", csenv.source_info.get_filepath()]
-        cmdv.extend((
-            "{}={}".format(k, v)
-            for k, v in csenv.source_info.iter_make_vars()
-        ))
-
-        argv = []
-        if subtype is not None:
-            argv.append(subtype)
-
-        argv.extend(args)
-        if not argv:
-            argv.append("defconfig")
-        # --
-
-        # TODO:
-        # if source_info.supports_out_of_tree_build():
-        #    make O=csenv.get_tmpdir().get_new_subdir()
+        # FIXME:
+        # if subtype == defconfig or not subtype:
+        #    get defconfig name from source info
+        #     args may contain further specifications of the defconfig
+        #     or a target name
         #
+        #  else
+        #     error
+        #  end if
 
-        cfg_file = csenv.source_info.get_filepath(".config")
-
-        fs.prepare_output_file(cfg_file, move=True)
-        # be *extra* sure that cfg_file does not exist
-        try:
-            os.unlink(cfg_file)
-        except OSError:
-            if os.path.lexists(cfg_file):
-                raise
-
-        with subproc.SubProc(cmdv + argv, logger=self.logger) as proc:
-            if not proc.join():
-                raise exc.ConfigurationSourceError("make-config failed")
+        if subtype:
+            self.make_target = subtype
+            args_rem = args
+        else:
+            raise NotImplementedError("more advanced make target")
+            args_rem = None
         # --
 
-        if not os.path.isfile(cfg_file):
-            raise exc.ConfigurationSourceError(
-                "make command did not create .config file"
-            )
+        return args_rem
+    # --- end of __init__ (...) ---
 
-        return cfg_file
+    def do_parse_source_argv(self, argv):
+        arg_config = _base.ConfigurationSourceArgConfig()
+        arg_config.argv.extend(argv)
+
+        arg_config.out_of_tree = False          # new attr
+        arg_config.outconfig = None             # new attr
+        arg_config.outconfig_name = ".config"   # new attr
+
+        if not self.senv.source_info.check_supports_out_of_tree_build():
+            arg_config.outconfig = (
+                self.senv.source_info.get_filepath(arg_config.outconfig_name)
+            )
+            arg_config.outfiles.append(arg_config.outconfig)
+
+        else:
+            arg_config.tmpdir = True
+            arg_config.out_of_tree = True
+        # --
+
+        return arg_config
     # ---
+
+    def do_prepare(self, arg_config):
+        super().do_prepare(arg_config)
+
+        if arg_config.out_of_tree:
+            if __debug__:
+                assert not arg_config.outfiles
+                assert arg_config.tmpdir
+                assert arg_config.tmpdir is not True
+            # --
+
+            # not necessary to backup-move/remove tmpfile
+            arg_config.outconfig = os.path.join(
+                arg_config.tmpdir, arg_config.outconfig_name
+            )
+        # --
+
+    def create_cmdv(self, arg_config):
+        def gen_make_var_args(mvar_items):
+            for vname, val in mvar_items:
+                yield "{!s}={!s}".format(vname, val)
+        # ---
+
+        def add_make_vars(mvar_items):
+            nonlocal cmdv
+            cmdv.extend(gen_make_var_args(mvar_items))
+        # --
+
+        # make -C <srctree>
+        cmdv = ["make", "-C", self.senv.source_info.get_filepath()]
+        #  + base argv
+        cmdv.extend(self.base_argv)
+
+        #  + make target
+        cmdv.append(self.make_target)
+
+        #  + ARCH=...,
+        add_make_vars(self.senv.source_info.iter_make_vars())
+
+        if arg_config.out_of_tree:
+            #  + O= ...
+            add_make_vars(
+                self.senv.source_info.iter_out_of_tree_build_make_vars(
+                    arg_config.tmpdir
+                )
+            )
+        # --
+
+        #  + arg_config.argv (if non-empty)
+        if arg_config.argv:
+            cmdv.extend(arg_config.argv)
+
+        return cmdv
+    # ---
+
+    def create_conf_basis(self, arg_config, proc):
+        return self.create_conf_basis_for_file(arg_config.outconfig)
 # --- end of MakeConfigurationSource ---
