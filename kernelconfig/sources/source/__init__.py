e9277e581857daf4ebc1a2fa6780c27c07f6cd8c
André Erdmann <dywi@mailerd.de>
2016-06-28 17:04:36 +0200

    add "local file" config source type

--- kernelconfig/sources/source/__init__.py
+++ kernelconfig/sources/source/__init__.py
@@ -1,9 +1,11 @@
 # This file is part of kernelconfig.
 # -*- coding: utf-8 -*-
 
+from .locfile import LocalFileConfigurationSource
 from .mk import MakeConfigurationSource
 
 
 __all__ = [
-    "MakeConfigurationSource"
+    "LocalFileConfigurationSource",
+    "MakeConfigurationSource",
 ]
