72c345cfe4e5f79807f9dfd8b0cbe4ee08bf4ccc
André Erdmann <dywi@mailerd.de>
2016-06-29 13:15:20 +0200

    config sources, _argconfig: __doc__

--- kernelconfig/sources/source/_argconfig.py
+++ kernelconfig/sources/source/_argconfig.py
@@ -35,8 +35,19 @@ class ConfigurationSourceArgConfig(object):
     # no __slots__ here! -- "consumers may add new attrs freely"
 
     class Outfile(object):
+        """An output file whose path is known at initialization time.
+
+        @param path:  path to the file (None if unknown, which should only
+                      be used by subclasses)
+        @type  path:  C{str} or C{None}
+        """
 
         def get_key(self):
+            """Returns a unique identifier for the outfile.
+
+            @return: unique identifier
+            @rtype:  C{str}
+            """
             return self.path
 
         def __hash__(self):
@@ -47,16 +58,36 @@ class ConfigurationSourceArgConfig(object):
             self.path = os.path.normpath(path) if path else path
 
         def get_path(self):
+            """
+            @raises AttributeError:  if path is not set
+
+            @return:  path to outfile
+            @rtype:   C{str}
+            """
             path = self.path
             if not path:
                 raise AttributeError("path is not set")
             return path
 
         def assign_tmpdir(self, tmpdir):
+            """
+            Assigns the outfile to a temporary directory,
+            modifying the outfile's path.
+
+            For this class, this is a no-op, because the path does not depend
+            on a tmpdir. Derived classes may vary.
+            """
             pass
     # ---
 
     class TmpOutfile(Outfile):
+        """
+        An output file whose path is unknown until a temporary directory
+        has been assigned to the config source arg config object.
+
+        @ivar name:  name of the file (path relative to tmpdir)
+        @type name:  C{str}
+        """
 
         def get_key(self):
             return self.name
@@ -79,24 +110,42 @@ class ConfigurationSourceArgConfig(object):
     # ---
 
     def iter_outfiles(self):
+        """
+        @return:  iterator over all outfile objects
+        """
         return iter(self._outfiles.values())
     # ---
 
     def iter_outfile_paths(self):
+        """
+        @return:  iterator over all outfile paths
+        """
         for outfile in self.iter_outfiles():
             yield outfile.get_path()
     # ---
 
     def iter_outconfig(self):
+        """
+        @return:  iterator over all outconfig objects
+        """
         return iter(self._outconfig.values())
     # ---
 
     def iter_outconfig_paths(self):
+        """
+        @return:  iterator over all outconfig paths
+        """
         for outfile in self.iter_outconfig():
             yield outfile.get_path()
     # ---
 
     def get_outconfig_path(self):
+        """
+        @raises AssertionError:  if there is not exactly one outconfig file
+
+        @return:  path to outconfig file
+        @rtype:   C{str}
+        """
         if len(self._outconfig) == 1:
             return next(iter(self._outconfig.values())).get_path()
         else:
@@ -107,6 +156,14 @@ class ConfigurationSourceArgConfig(object):
     # ---
 
     def _add_outconfig(self, outfile):
+        """Registers an outconfig file (w/o registering it in _outfiles).
+
+        @raises KeyError:  if file is already registered
+
+        @param outfile:  outfile object
+
+        @return:  outfile
+        """
         key = outfile.get_key()
         if key in self._outconfig:
             raise KeyError(key)
@@ -115,6 +172,16 @@ class ConfigurationSourceArgConfig(object):
     # ---
 
     def _add_outfile(self, outfile, is_outconfig=True):
+        """Registers an outfile, optionally also as outconfig file.
+
+        @raises KeyError:  if file is already registered
+
+        @param   outfile:       outfile object
+        @keyword is_outconfig:  whether to also register the outfile object
+                                in outconfig. Defaults to True.
+
+        @return:  outfile
+        """
         key = outfile.get_key()
 
         if key in self._outfiles:
@@ -131,37 +198,106 @@ class ConfigurationSourceArgConfig(object):
     # ---
 
     def add_outfile(self, path, is_outconfig=True):
+        """Adds an output file.
+
+        Output files may or may not be .config files.
+        They are expected to be created by the configuration source,
+        and are thus subject to removal/backup-moving (prepare_output_file()).
+
+        @param path:            path to outfile
+        @keyword is_outconfig:  whether outfile is an output config file
+                                Defaults to True.
+
+        @return:  outfile object
+        """
         return self._add_outfile(
             self.Outfile(path), is_outconfig=is_outconfig
         )
     # --- end of add_outfile (...) ---
 
     def add_tmp_outfile(self, name, is_outconfig=True):
+        """Adds a temporary output file.
+
+        The file's path is not known until a temporary directory
+        has been assigned.
+
+        @param name:            name of the outfile
+        @keyword is_outconfig:  whether outfile is an output config file
+                                Defaults to True.
+
+        @return:  outfile object
+        """
         outfile = self._add_outfile(
             self.TmpOutfile(name), is_outconfig=is_outconfig
         )
-        self.set_need_tmpdir()
+
+        if self.has_tmpdir():
+            outfile.assign_tmpdir(self._tmpdir)
+        else:
+            self.set_need_tmpdir()
+
         return outfile
     # --- end of add_tmp_outfile (...) ---
 
     def add_outconfig(self, path):
+        """Adds an output config file.
+
+        Output config files should be .config files.
+        They may already exist,
+        and should not be removed/backup-moved unless they have also been
+        added as "outfile", in which case add_outfile(path, is_outconfig=True)
+        should be used instead.
+
+        @param name:  path to the outconfig file
+
+        @return:  outfile object
+        """
         return self._add_outconfig(self.Outfile(path))
     # --- end of add_outconfig (...) ---
 
     def has_tmpdir(self):
+        """
+        Returns True if a tmpdir has been assigned, and False otherwise.
+
+        The return value is also False if a tmpdir has been requested,
+        but not assigned yet.
+        """
         return self._tmpdir and self._tmpdir is not True
     # --- end of has_tmpdir (...) ---
 
     def check_need_tmpdir(self):
+        """
+        Returns True if a tmpdir should be assigned, and False otherwise.
+        """
         return (self._tmpdir is True)
     # --- end of check_need_tmpdir (...) ---
 
     def set_need_tmpdir(self):
+        """
+        Requests that a tmpdir should be assigned later on.
+
+        No-op if a tmpdir has already been assigned (or requested).
+
+        @return:  None (implicit)
+        """
         if not self._tmpdir:
             self._tmpdir = True
     # --- end of set_need_tmpdir (...) ---
 
     def assign_tmpdir(self, tmpdir):
+        """
+        Assigns a tmpdir, should only be called after check_need_tmpdir():
+
+        >>> if obj.check_need_tmpdir():
+        >>>    obj.assign_tmpdir(...)
+
+        Also takes care of settings the path of the temporary outfiles.
+
+        @param tmpdir:  path to tmpdir
+        @type  tmpdir:  C{str}
+
+        @return:  None (implicit)
+        """
         if tmpdir is True or not tmpdir:
             # can only change tmpdir from None/True to non-empty path
             raise ValueError(tmpdir)
@@ -176,6 +312,12 @@ class ConfigurationSourceArgConfig(object):
     # --- end of assign_tmpdir (...) ---
 
     def _get_tmpdir(self):
+        """
+        @raises AttributeError:  if tmpdir requested but not assigned
+
+        @return:  None or path to tmpdir
+        @rtype:   C{None} or C{str}
+        """
         if self._tmpdir is True:
             raise AttributeError("referencing tmpdir=True is not allowed")
         else:
