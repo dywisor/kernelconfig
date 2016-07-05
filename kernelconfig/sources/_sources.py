522a907a4e957b02af4a5795dbeeedb4480b8115
André Erdmann <dywi@mailerd.de>
2016-07-05 15:50:37 +0200

    conf sources: use sourcetype, verify arch

--- kernelconfig/sources/_sources.py
+++ kernelconfig/sources/_sources.py
@@ -1,45 +1,20 @@
 # This file is part of kernelconfig.
 # -*- coding: utf-8 -*-
 
-import abc
-import enum
 import os.path
 import shlex
 
 from .abc import sources as _sources_abc
 from .abc import exc
-from . import source as _source
 from . import sourcedef
+from . import sourcetype
 from . import sourceenv
 
 
-__all__ = ["ConfigurationSourceType", "ConfigurationSources"]
+__all__ = ["ConfigurationSources"]
 
 
-@enum.unique
-class ConfigurationSourceType(enum.IntEnum):
-    (
-        s_unknown,
-        s_file,
-        s_command,
-        s_make,
-        s_script,
-        s_source
-    ) = range(6)
-
-    def get_name(self):
-        return self.name[2:]
-
-    __str__ = get_name
-
-# ---
-
-
-class ConfigurationSourcesBase(_sources_abc.AbstractConfigurationSources):
-
-    @abc.abstractproperty
-    def SOURCE_TYPE_KEYWORDS(cls):
-        raise NotImplementedError()
+class ConfigurationSources(_sources_abc.AbstractConfigurationSources):
 
     def __init__(self, install_info, source_info, **kwargs):
         super().__init__(**kwargs)
@@ -49,7 +24,7 @@ class ConfigurationSourcesBase(_sources_abc.AbstractConfigurationSources):
             source_info=source_info
         )
 
-    def _create_source_by_name_from_files(
+    def _create_curated_source_def_by_name_from_files(
         self, source_name, source_def_file, source_script_file
     ):
         self.logger.debug("Initializing curated source %s", source_name)
@@ -58,65 +33,93 @@ class ConfigurationSourcesBase(_sources_abc.AbstractConfigurationSources):
             conf_source_env=self.senv,
             name=source_name,
             parent_logger=self.logger,
-            source_def_file=source_def_file
+            source_def_file=source_def_file,
+            source_script_file=source_script_file
         )
 
-        source_type_name = source_def.get("type", "").strip()
-        if not source_type_name:
-            # autodetect
-            if source_script_file:
-                source_type_name = "script"
+        try:
+            source_type = source_def.get_source_type()
 
-            # failing that, pick the most appropriate exception type
-            elif source_def_file:
-                raise exc.ConfigurationSourceInvalidError(
-                    "source {} with unknown type and no script file".format(
-                        source_name
-                    )
-                )
+        except exc.ConfigurationSourceMissingType:
+            # failed to detect source type
+            if source_def_file:
+                # raising same, but new exception? FIXME
+                raise exc.ConfigurationSourceMissingType(
+                    "source {} has unknown type".format(source_name)
+                ) from None
             else:
-                raise exc.ConfigurationSourceNotFound(source_name)
+                raise exc.ConfigurationSourceNotFound(source_name) from None
+
+        except KeyError:
+            # if there is no source type matching source_type_name,
+            #   then the type has to be guessed
+            #   however, this should have already been done
+            #   ('if not source_def.get("type")'),
+            #   so it indicates an error here
+            #
+            raise exc.ConfigurationSourceInvalidError(
+                "{}: could not detect source type".format(source_name)
+            ) from None
         # --
 
-        # add "scriptfile" if not already in source_def (but possibly None)
-        source_def.setdefault("scriptfile", source_script_file)
-
-        source_type, source_subtype, source_cls = (
-            self.get_source_type(source_type_name)
-        )
-
-        if source_type is ConfigurationSourceType.s_source:
+        if source_type.is_source():
             # type of source is source => error
             raise exc.ConfigurationSourceInvalidError(
                 "{}: type of source must not be 'source'".format(source_name)
             )
 
-        elif source_subtype:
+        elif source_type.source_subtype:
             # couldfix - shouldfix
             raise exc.ConfigurationSourceInvalidError(
                 "{}: subtypes are not supported: {}".format(
-                    source_name, source_subtype
+                    source_name, source_type.source_subtype
                 )
             )
 
-        elif source_type is None or source_cls is None:
-            # if source_type is None, then the type has to be guessed
-            #   however, this should have already been done
-            #   ('if not source_def.get("type")'),
-            #   so it indicates an error here
-            #
+        elif source_type.source_cls is None:
             # if source_cls is None, then the type has no class and
             # it cannot be handled here.
             # (this should be an redundant case, since source_cls==None
-            # implies a source_type of either None or "source")
+            # implies a no source_type or a "source" source_type)
             #
             raise exc.ConfigurationSourceInvalidError(
-                "{}: could not detect source type".format(source_name)
+                "{}: has no source type class".format(source_name)
             )
         # --
 
         self.logger.debug("%s is a %s source", source_name, source_type)
-        return source_cls.new_from_def(
+
+        return (source_def, source_type)
+    # --- end of _create_curated_source_def_by_name_from_files (...) ---
+
+    def _create_source_by_name_from_files(
+        self, source_name, source_def_file, source_script_file
+    ):
+        source_def, source_type = (
+            self._create_curated_source_def_by_name_from_files(
+                source_name, source_def_file, source_script_file
+            )
+        )
+
+        if not source_def.arch:
+            # Debatable if this is an init-time or
+            # a get_configuration_basis()-time error.
+            #
+            # It makes no difference where the error is raised, and there is
+            # simply no point in creating unusable source objects,
+            # so fail early.
+            raise exc.ConfigurationSourceArchNotSupported(
+                source_name,
+                archs=(
+                    name for _, name
+                    in self.senv.source_info.iter_target_arch_dedup()
+                ),
+                supported_archs=source_def.get("architectures")
+            )
+
+        # --
+
+        return source_type.source_cls.new_from_def(
             name=source_name,
             conf_source_env=self.senv,
             source_def=source_def,
@@ -198,22 +201,23 @@ class ConfigurationSourcesBase(_sources_abc.AbstractConfigurationSources):
 
         source_def = shlex.split(source_def_str)
 
-        source_cls = None
+        source_type = None  # set below
         source_args = None
         # source_data is already set
-        source_subtype = None
 
         # fixme: file uri
         if os.path.isabs(source_def[0]):
             # implicit file
-            source_cls = _source.LocalFileConfigurationSource
+            source_type = sourcetype.get_source_type("local_file")
             source_args = source_def
 
         else:
             source_args = source_def[1:]
-            source_type, source_subtype, source_cls = (
-                self.get_source_type(source_def[0])
-            )
+            try:
+                source_type = sourcetype.get_source_type(source_def[0])
+            except KeyError:
+                raise
+                source_type = None
 
             if source_type is None:
                 # then the type is implicit, and we have to "guess":
@@ -226,24 +230,26 @@ class ConfigurationSourcesBase(_sources_abc.AbstractConfigurationSources):
                 #
                 # TODO
                 raise NotImplementedError("guess typeof", source_def)
-
-            elif source_type is ConfigurationSourceType.s_source:
-                # calling convention different from source_cls.new_from*
-                return self.create_source_by_name_from_settings(
-                    source_subtype, source_args, source_data
-                )
-
-            elif source_cls is None:
-                raise NotImplementedError(
-                    "missing source cls for type {}".format(source_type)
-                )
+            # --
         # --
 
-        return source_cls.new_from_settings(
-            conf_source_env=self.senv,
-            subtype=source_subtype, args=source_args, data=source_data,
-            parent_logger=self.logger
-        )
+        if source_type.is_source():
+            # calling convention different from source_cls.new_from*
+            return self.create_source_by_name_from_settings(
+                source_type.source_subtype, source_args, source_data
+            )
+
+        elif source_type.source_type is None:
+            raise NotImplementedError(
+                "missing source cls for type {}".format(source_type)
+            )
+        else:
+            return source_type.source_cls.new_from_settings(
+                conf_source_env=self.senv,
+                subtype=source_type.source_subtype,
+                args=source_args, data=source_data,
+                parent_logger=self.logger
+            )
     # --- end of get_configuration_source_from_settings (...) ---
 
     def get_configuration_basis_from_settings(self, settings):
@@ -254,64 +260,4 @@ class ConfigurationSourcesBase(_sources_abc.AbstractConfigurationSources):
         return conf_source.get_configuration_basis(conf_args)
     # --- end of get_configuration_basis_from_settings (...) ---
 
-    def get_source_type(self, source_type_name):
-        slow = source_type_name.lower()
-
-        try:
-            source_type_entry = self.SOURCE_TYPE_KEYWORDS[slow]
-        except KeyError:
-            source_type_entry = (None, None)
-
-        source_type, source_subtype = source_type_entry
-
-        source_cls = None
-
-        if source_type is None:
-            assert source_subtype is None
-            source_cls = None
-
-        elif source_type is ConfigurationSourceType.s_file:
-            source_cls = _source.FileConfigurationSource
-
-        elif source_type is ConfigurationSourceType.s_command:
-            source_cls = _source.CommandConfigurationSource
-
-        elif source_type is ConfigurationSourceType.s_make:
-            # make command w/ target source_subtype
-            source_cls = _source.MakeConfigurationSource
-
-        elif source_type is ConfigurationSourceType.s_source:
-            source_cls = None
-
-        elif source_type is ConfigurationSourceType.s_script:
-            # embedded script w/ lang source_subtype
-            source_cls = _source.ScriptConfigurationSource
-
-        else:
-            raise NotImplementedError(
-                "unknown source type {}".format(source_type)
-            )
-
-        return (source_type, source_subtype, source_cls)
-    # --- end of get_source_type (...) ---
-
 # --- end of ConfigurationSourcesBase ---
-
-
-class ConfigurationSources(ConfigurationSourcesBase):
-
-    SOURCE_TYPE_KEYWORDS = {
-        "file":         (ConfigurationSourceType.s_file, None),
-
-        "cmd":          (ConfigurationSourceType.s_command, None),
-        "command":      (ConfigurationSourceType.s_command, None),
-
-        "mk":           (ConfigurationSourceType.s_make, None),
-        "make":         (ConfigurationSourceType.s_make, None),
-        "defconfig":    (ConfigurationSourceType.s_make, "defconfig"),
-
-        "script":       (ConfigurationSourceType.s_script, None),
-        "sh":           (ConfigurationSourceType.s_script, "sh"),
-
-        "source":       (ConfigurationSourceType.s_source, None)
-    }
