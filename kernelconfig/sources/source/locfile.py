94c9b97ea2a4f2115eb71519a12a9ac963f8e4ae
André Erdmann <dywi@mailerd.de>
2016-07-06 12:38:32 +0200

    "local file" conf source: init_from_def()

--- kernelconfig/sources/source/locfile.py
+++ kernelconfig/sources/source/locfile.py
@@ -1,7 +1,7 @@
 # This file is part of kernelconfig.
 # -*- coding: utf-8 -*-
 
-import os.path
+import os
 
 from . import _sourcebase
 from ..abc import exc
@@ -24,9 +24,67 @@ class LocalFileConfigurationSource(_sourcebase.ConfigurationSourceBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.filepath = None
+        self.filepath_is_dynamic = None
+
+    def check_source_valid(self):
+        if not self.filepath:
+            raise exc.ConfigurationSourceInvalidError("empty filepath")
+
+        elif (
+            not self.filepath_is_dynamic
+            and not os.access(self.filepath, os.R_OK)
+        ):
+            raise exc.ConfigurationSourceInvalidError(
+                "config file does not exist or is not readable: {!r}".format(
+                    self.filepath
+                )
+            )
+        # --
+    # --- end of check_source_valid (...) ---
 
     def add_auto_var(self, varname, varkey):
         return False
+
+    def _set_file_uri(self, file_uri_arg, allow_dynamic_uri):
+        """
+        @param file_uri_arg:       (input) file uri
+        @type  file_uri_arg:       C{str}
+        @param allow_dynamic_uri:  whether dynamic file uris should be allowed
+                                   or not.
+                                   Dynamic file uris are get str-formatted
+                                   after argparsing in get_configuration_basis
+        @type  allow_dynamic_uri:  C{bool}
+
+        @return:  None (implicit)
+        """
+        str_formatter = self.get_str_formatter()
+        try:
+            file_uri = str_formatter.format(file_uri_arg)
+
+        except (KeyError, IndexError):
+            if allow_dynamic_uri:
+                file_uri = file_uri_arg
+                file_uri_is_dynamic = True
+            else:
+                raise exc.ConfigurationSourceInvalidError(
+                    "file uri needs dynamic str-formatting", file_uri_arg
+                )
+
+        else:
+            file_uri_is_dynamic = False
+        # --
+
+        # --- copy of fileuri.py, _set_file_uri() ends here ---
+
+        self.filepath_is_dynamic = file_uri_is_dynamic
+        if file_uri_is_dynamic:
+            self.filepath = file_uri
+        else:
+            # normpath() for eliminating redundant leading slashes
+            #  this is an limitation caused by how "local file" source types
+            #  are created from settings (they must start with "/")
+            self.filepath = os.path.normpath(file_uri)
+    # --- end of _set_file_uri (...) ---
 
     def init_from_settings(self, subtype, args, data):
         if data:
@@ -43,31 +101,51 @@ class LocalFileConfigurationSource(_sourcebase.ConfigurationSourceBase):
             raise exc.ConfigurationSourceInvalidError("too many args")
         # --
 
-        str_formatter = self.get_str_formatter()
-
-        # normpath() for eliminating redundant leading slashes
-        #  this is an limitation caused by how "local file" source types
-        #  are created from settings (they must start with "/")
-        self.filepath = os.path.normpath(str_formatter.format(args[0]))
+        self._set_file_uri(args[0], False)
         return []
-    # ---
+    # --- end of init_from_settings (...) ---
+
+    def init_from_def(self, source_def):
+        super().init_from_def(source_def)
+
+        file_uri_arg = source_def.get("path")
+        if file_uri_arg:
+            self._set_file_uri(file_uri_arg, True)
+            # else check_source_valid() will complain about it
+    # --- end of init_from_def (...) ---
 
     def get_configuration_basis(self, args):
-        if args:
-            raise exc.ConfigurationSourceInvalidError("non-empty args")
+        fmt_vars = {}
+
+        if self.arg_parser is not None:
+            # allow None args
+            params = self.arg_parser.parse_args(args or [])
+            # code dup!
+            fmt_vars.update((
+                ("param_{}".format(name.lower()), (value or ""))
+                for name, value in vars(params).items()
+            ))
+
+        elif args:
+            raise exc.ConfigurationSourceFeatureUsageError(
+                "this configuration source does not accept parameters"
+            )
         # --
 
-        filepath = self.filepath
+        if self.filepath_is_dynamic:
+            str_formatter = self.get_str_formatter()
+            # this behaves slightly different than the 'dynamic' str formatter
+            # from PhasedConfigurationSourceBase,
+            # uppercase/mixedcase param_ vars do not get recognized
+            # and lead to KeyErrors
+            filepath = str_formatter.vformat(self.filepath, (), fmt_vars)
+        else:
+            filepath = self.filepath
+        # --
 
         if os.path.isabs(filepath):
             outconfig = filepath
         else:
-            # there is currently no way
-            # to create LocalFileConfigurationSource objects with a relpath
-            raise AssertionError(
-                "unreachable code: LocalFileConfigurationSource with relpath"
-            )
-
             outconfig = self.senv.get_config_file_path(filepath)
             if not outconfig:
                 raise exc.ConfigurationSourceNotFound("files", filepath)
