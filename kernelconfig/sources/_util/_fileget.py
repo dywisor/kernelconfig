e0b20703f17bde50d96e12420c41837dc4fd37af
André Erdmann <dywi@mailerd.de>
2016-07-11 20:30:07 +0200

    conf source fileget: replace wrapper with proper funcs
    get_file_write_to_file() did not pass the correct url to FileGetError.

--- kernelconfig/sources/_util/_fileget.py
+++ kernelconfig/sources/_util/_fileget.py
@@ -1,8 +1,6 @@
 # This file is part of kernelconfig.
 # -*- coding: utf-8 -*-
 
-import functools
-
 from ..abc import exc
 from ...util import fileget as _orig_fileget
 
@@ -10,19 +8,20 @@ from ...util import fileget as _orig_fileget
 __all__ = ["get_file", "get_file_write_to_file"]
 
 
-def _fileget_exception_rewrite(func):
-    def wrapper(url, *args, **kwargs):
-        try:
-            return func(url, *args, **kwargs)
+def get_file(url, data=None, **kwargs):
+    try:
+        return _orig_fileget.get_file(url, data=data, **kwargs)
 
-        except (OSError, IOError):
-            raise exc.ConfigurationSourceFileGetError(url) from None
-    # ---
-
-    return functools.update_wrapper(wrapper, func)
-# --- end of _fileget_exception_rewrite (...) ---
+    except (OSError, IOError):
+        raise exc.ConfigurationSourceFileGetError(url) from None
+# ---
 
 
-get_file = _fileget_exception_rewrite(_orig_fileget.get_file)
-get_file_write_to_file = \
-    _fileget_exception_rewrite(_orig_fileget.get_file_write_to_file)
+def get_file_write_to_file(filepath, url, data=None, **kwargs):
+    try:
+        return _orig_fileget.get_file_write_to_file(
+            filepath, url, data=data, **kwargs
+        )
+    except (OSError, IOError):
+        raise exc.ConfigurationSourceFileGetError(url) from None
+# ---
