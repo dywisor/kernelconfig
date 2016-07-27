392a41d2579c85c0112b43e8750b0e69b91d76df
André Erdmann <dywi@mailerd.de>
2016-07-27 22:40:09 +0200

    add util.osmisc: get cpu count, get free disk space

---
+++ kernelconfig/util/osmisc.py
@@ -0,0 +1,39 @@
+# This file is part of kernelconfig.
+# -*- coding: utf-8 -*-
+
+__all__ = [
+    "get_cpu_count",
+    "get_free_space",
+    "get_free_space_m",
+]
+
+import os
+
+if hasattr(os, "cpu_count"):
+    # Python >= 3.4
+    get_cpu_count = os.cpu_count
+
+elif hasattr(os, "sched_getaffinity"):
+    # Python 3.3
+    def get_cpu_count():
+        return len(os.sched_getaffinity(0))
+
+else:
+    # Python < 3.3
+    #  Since the minimum Python version is 3.3, this block is not implemented.
+
+    def get_cpu_count():
+        # try multiprocessing.cpu_count(), catch NotImplementedError
+        raise NotImplementedError()
+# --
+
+
+def get_free_space(filepath):
+    vfs_info = os.statvfs(filepath)
+    return (vfs_info.f_frsize * vfs_info.f_bavail)
+# --- end of get_free_space (...) ---
+
+
+def get_free_space_m(filepath):
+    return get_free_space(filepath) // (1024 * 1024)
+# --- end of get_free_disk_space_m (...) ---
