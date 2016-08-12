f62f7651a64c7771dd06f17e73b08cbd5431c765
André Erdmann <dywi@mailerd.de>
2016-08-12 17:42:25 +0200

    mk/install.mk: add doins_norecur()
    Installs all files from a dir, non-recursive.

--- mk/install.mk
+++ mk/install.mk
@@ -35,3 +35,17 @@ f_install_files_recursive = $(call \
 	_f_install_files_recursive,$(strip $(1)),$(strip $(2)),$(strip $(3)))
 
 doins_recursive = $(call f_install_files_recursive,$(INSMODE),$(1),$(2))
+
+# _f_install_files_norecur(ins_mode, src_root, dst_root)
+define _f_install_files_norecur
+	( cd '$(2:/=)/' && find ./ -mindepth 1 -maxdepth 1 -type f; ) | \
+		sed -e 's=^[.]/==' | \
+		xargs -r -n 1 -I '{}' \
+			$(INSTALL) -D -m '$(1)' --  '$(2:/=)/{}' '$(3:/=)/{}'
+endef
+
+# f_install_files_norecur(ins_mode, src_root, dst_root)
+f_install_files_norecur = $(call \
+	_f_install_files_norecur,$(strip $(1)),$(strip $(2)),$(strip $(3)))
+
+doins_norecur = $(call f_install_files_norecur,$(INSMODE),$(1),$(2))
