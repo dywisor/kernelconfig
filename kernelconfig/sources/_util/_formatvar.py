58887dcdeea1afbafd011bbee49e6432fe50f48a
André Erdmann <dywi@mailerd.de>
2016-08-16 16:43:40 +0200

    sources._util._formatvar: __doc__

--- kernelconfig/sources/_util/_formatvar.py
+++ kernelconfig/sources/_util/_formatvar.py
@@ -15,6 +15,19 @@ __all__ = [
 
 
 class _ConfigurationSourceAutoVarMapping(collections.abc.Sized):
+    """
+    A collection of automatic variables, used for dynamic str-formatting.
+    While it is a mapping-like data structure,
+    it does not provide the usual dict-access methods like __getitem__().
+
+    Instead, entries must be created with add() or add_alias(),
+    and the complete format var mapping can be created with get_vars().
+
+    @ivar data:       mapping from outfile identifier to outfile object
+    @type data:       C{dict} :: hashable => sub-of L{AbstractOutfile}
+    @ivar alias_map:  mapping from outfile name to outfile identifier
+    @type alias_map:  C{dict} :: C{str} => hashable
+    """
 
     def __init__(self):
         super().__init__()
@@ -29,20 +42,80 @@ class _ConfigurationSourceAutoVarMapping(collections.abc.Sized):
 
     @abc.abstractmethod
     def create_new_auto_var(self, key):
+        """Creates a new automatic variable.
+
+        Derived classes must implement this method.
+
+        @param key:  auto var identifier
+        @type  key:  hashable
+
+        @return:  auto var
+        @rtype:   subclass of L{AbstractOutfile}
+        """
         raise NotImplementedError()
 
     @abc.abstractmethod
     def get_vars(self):
+        """
+        Creates a format varname => outfile mapping for all outfiles,
+        including aliases and returns them as 2-tuple
+        (deduplicated list of outfiles, format var X outfile mapping).
+        The returned outfiles are copies and can be modified freely.
+
+        Not injective: due to aliases, multiple format variable names may
+                       reference the same outfile object,
+                       so, generally, len(fmt_vars) >= len(outfiles).
+                       For that reason, a duplicate-free list of outfile
+                       objects is also returned.
+
+        Derived classes must implement this method.
+
+        @return:  2-tuple (
+                     list of new, unique outfile objects,
+                     format varname => outfile object mapping
+                  )
+        @rtype:   2-tuple (
+                     C{list} of sub-of L{AbstractOutfile},
+                     C{dict} :: C{str} => L{AbstractOutfile}
+                  )
+        """
         raise NotImplementedError()
 
     def add(self, key, alias_key):
+        """
+        Adds an auto var to the mapping, creates it if necessary.
+
+        @raises KeyError:  duplicate alias entry that does not reference
+                           the same auto var
+
+        @param key:        the auto var's identifier
+        @type  key:        hashable
+        @param alias_key:  name of the auto var
+        @type  alias_key:  C{str}
+        """
         if key not in self.data:
             self.data[key] = self.create_new_auto_var(key)
 
+        # the get_vars() implementation looks up names in self.alias_map only,
+        # so an alias entry needs to be created even if key == alias_key
         self.add_alias(key, alias_key)
     # ---
 
     def add_alias(self, key, alias_key):
+        """
+        Adds an alias entry to the mapping,
+        does not check whether the referenced entry does actually exist.
+
+        @raises KeyError:  duplicate alias entry that does not reference
+                           the same auto var
+
+        @param key:        the auto var's identifier
+        @type  key:        hashable
+        @param alias_key:  name of the auto var
+        @type  alias_key:  C{str}
+
+        @return:  None (implicit)
+        """
         if alias_key in self.alias_map:
             if self.alias_map[alias_key] != key:
                 raise KeyError(alias_key)
